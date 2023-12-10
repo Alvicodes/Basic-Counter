07208cfa797896586bb21cc9d9382f7ef0d3a6c3
alvcodec <148978672+alvcodec@users.noreply.github.com>
2023-12-10 11:40:05 +0000

    Updated CSS and JS

--- script.js
+++ script.js
@@ -7,9 +7,18 @@ let count = 0;
 
 addBtn.addEventListener('click', () => {
     count++;
-    counter.innerText = count;   
+    counter.innerText = count;
+    counter.style.backgroundColor =  "rgba(127, 255, 212, 0.155)";   
 })
 minusBtn.addEventListener('click', () => {
     count--;
-    counter.innerText = count;   
+    counter.innerText = count;
+    counter.style.backgroundColor = "rgba(221, 160, 221, 0.155)";   
+   
+})
+resetBtn.addEventListener('click', () => {
+    count = 0;
+    counter.innerText = count;
+    counter.style.backgroundColor = "aliceblue";   
+   
 })
